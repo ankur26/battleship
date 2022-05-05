3f2d59beb8d82047383bfff9205fec5c6ba62e86
Ankur Bhatkalkar <ankur26@users.noreply.github.com>
2022-05-05 03:29:12 -0400

    Added more tests and functions for Player

--- src/player.js
+++ src/player.js
@@ -1,9 +1,25 @@
+const { EvalSourceMapDevToolPlugin } = require('webpack');
 const gameboard = require('./gameboard');
-const Player = (name="Computer") =>{
-    let playerGameboard = gameboard();
-    const updateOrientation=()=> playerGameboard.changeOrientation();
-    const getNamedOrientation =()=> playerGameboard.getOrientation() ? "Horizontal" : "Vertical";
-    return {name,updateOrientation,getNamedOrientation};
-}
+const Player = (name = 'Computer') => {
+	let playerGameboard = gameboard();
+	const updateOrientation = () => playerGameboard.changeOrientation();
+	const getNamedOrientation = () => (playerGameboard.getOrientation() ? 'Horizontal' : 'Vertical');
+	const getName = () => name;
+	const takeAttack = (x, y) => {
+        try {
+            return playerGameboard.receiveAttack(x, y);
+        } catch (e) {
+            throw new Error(e);
+        }
+	};
+	const setShip = (x, y, length) => {
+		try {
+			return playerGameboard.placeShip(x, y, length);
+		} catch (e) {
+			throw new Error(e);
+		}
+	};
+	return { getName, updateOrientation, getNamedOrientation, setShip, takeAttack };
+};
 
 module.exports = Player;
--- test/player.test.js
+++ test/player.test.js
@@ -2,11 +2,11 @@ const Player = require('../src/player');
 
 
 test('should return default name', () => { 
-    expect(Player().name).toBe('Computer');
+    expect(Player().getName()).toBe('Computer');
  });
 
 test('should update name if given one', () => { 
-    expect(Player('Ankur').name).toBe('Ankur');
+    expect(Player('Ankur').getName()).toBe('Ankur');
  });
 
 test('should return horizontal oritentation as defaul', () => { 
@@ -17,4 +17,19 @@ test('should get new orientation after update', () => {
     let p = Player();
     p.updateOrientation();
     expect(p.getNamedOrientation()).toBe('Vertical');
- })
+ });
+
+
+let player1 = Player("Player 1");
+let player2 = Player();
+
+test('should add ships without any issues', () => { 
+  expect(player1.setShip(0,0,5)).toEqual({valid:true, coordinates:[]});
+  expect(player2.setShip(0,0,5)).toEqual({valid:true,coordinates:[]});
+ });
+
+test('should take an attack and receive attacks', ()=>{
+    expect(player1.takeAttack(0,0)).toEqual({hit:true,gameOver:false});
+    expect(player2.takeAttack(0,0)).toEqual({hit:true,gameOver:false});
+
+})
