fa417b33191f7cfd0406e43764a21153b4e473df
ankur <ankur@Xubuntu.myguest.virtualbox.org>
2023-01-01 13:06:52 -0500

    Reworked gameboard added tests for it

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,30 +1,69 @@
+import ship from "./ship";
+
 const gameboard = () => {
   const grid = [];
   const ships = [];
   for (let i = 0; i < 10; i++) {
     let row = [];
     for (let j = 0; j < 10; j++) {
-      row.push("E");
+      row.push({
+        empty: true,
+        hit: false,
+        shipPlaced: false,
+        missed: false,
+        ship_id: undefined,
+      });
     }
     grid.push(row);
   }
-  
+
   const getCellState = (row, col) => {
-    if (row && col) {
-      if (typeof row === "number" && typeof col === "number") {
-        if (row >= 10 || row < 0) {
-          throw new Error("Row cannot be less than 0 or greater than 9");
-        }
-        if (col >= 10 || col < 0) {
-          throw new Error("Column cannot be less than 0 or greater than 9");
-        }
-        return grid[row][col];
-      }
-      throw new TypeError("Row and col should have number types")
+    if (!grid[row][col]) {
+      throw new Error("Grid position is undefined or invalid");
     }
-    throw new Error("Row and/or column cannot be empty");
+    return grid[row][col];
   };
-  return {getCellState};
+
+  const checkOverlap = (row, col, placeAlongXaxis, length) => {
+    if (placeAlongXaxis) {
+      for (let i = col; i < col + length; i++) {
+        if (grid[row][i].shipPlaced) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      for (let i = row; i < row + length; i++) {
+        if (grid[i][col].shipPlaced) {
+          return true;
+        }
+      }
+      return false;
+    }
+  };
+  const placeShip = (row, col, placeAlongXaxis, length) => {
+    if (!checkOverlap(row, col, placeAlongXaxis, length)) {
+      ships.push(ship(length));
+      if (placeAlongXaxis) {
+        for (let i = col; i < col + length; i++) {
+          grid[row][i].shipPlaced = true;
+          grid[row][i].empty = false;
+          grid[row][i].ship_id = ships.length - 1;
+        }
+        return true;
+      } else {
+        for (let i = row; i < row + length; i++) {
+          grid[i][col].shipPlaced = true;
+          grid[i][col].empty = false;
+          grid[i][col].ship_id = ships.length - 1;
+        }
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return { getCellState, checkOverlap, placeShip };
 };
 
 export default gameboard;
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -2,44 +2,53 @@ import gameboard from "../src/gameboard";
 
 const g = gameboard();
 
-test("should get cell state for correct input", () => {
-  expect(g.getCellState(1, 1)).toBe("E");
+test("should get cell state", () => {
+  expect(g.getCellState(1, 1).empty).toBe(true);
 });
-test("should throw error for empty input", () => {
+test("should log an error on console for invalid input", () => {
   expect(() => {
     g.getCellState();
-  }).toThrow("Row and/or column cannot be empty");
-  expect(() => {
-    g.getCellState(1);
-  }).toThrow("Row and/or column cannot be empty");
-  expect(() => {
-    g.getCellState(undefined, 1);
-  }).toThrow("Row and/or column cannot be empty");
+  }).toThrow();
 });
-test("should check for type", () => {
-  expect(() => {
-    g.getCellState("1", "2");
-  }).toThrow("Row and col should have number types");
-  expect(() => {
-    g.getCellState(1, "2");
-  }).toThrow("Row and col should have number types");
-  expect(() => {
-    g.getCellState("1", 2);
-  }).toThrow("Row and col should have number types");
+
+test("should check for overlaps", () => {
+  expect(g.checkOverlap(1, 1, true, 2)).toBe(false);
 });
-test("should check for row range", () => {
-  expect(() => {
-    g.getCellState(-1, 1);
-  }).toThrow("Row cannot be less than 0 or greater than 9");
-  expect(() => {
-    g.getCellState(10, 1);
-  }).toThrow("Row cannot be less than 0 or greater than 9");
+
+test("should place a ship", () => {
+  expect(g.placeShip(0, 0, true, 5)).toBe(true);
 });
-test("should check for column range", () => {
-  expect(() => {
-    g.getCellState(1, -1);
-  }).toThrow("Column cannot be less than 0 or greater than 9");
-  expect(() => {
-    g.getCellState(1,10 );
-  }).toThrow("Column cannot be less than 0 or greater than 9");
+
+test("should fail overlaps for 0,0 to 0,4", () => {
+  for (let i = 0; i < 5; i++) {
+    expect(g.checkOverlap(0, i, true, 4)).toBe(true);
+  }
 });
+
+test("should be able to place a ship at another place vertically", () => {
+  expect(g.placeShip(4, 4, false, 4)).toBe(true);
+});
+
+test('should correctly check overlap across horizontal axes',()=>{
+  for(let i = 4; i<8;i++)
+  expect(g.checkOverlap(i,4,true,4)).toBe(true)
+});
+
+test('should correctly check for overlap for both orientations', () => { 
+  //This will test other rows than the actual placed ships
+  `
+  Currently there are two ships positioned as mentioned below
+  0,0 to 0,5 - we will test for overlap for columns 5-9 in the same row 
+  4,4 to 8,4 - we will test for overlap for rows 0-3 and 9 in the same column.
+  This also manages to test multi length ships.
+  `
+  for(let i = 5; i< 8; i++){
+    expect(g.checkOverlap(0,i,true,2)).toBe(false)
+  }
+  for(let i = 1; i<2;i++){
+    console.log(g.getCellState(i,4))
+    expect(g.checkOverlap(i,4,false,2)).toBe(false)
+  }
+  expect(g.checkOverlap(8,4,false,2)).toBe(false)
+ })
+
